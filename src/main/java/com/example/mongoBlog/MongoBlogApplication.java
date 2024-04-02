0b7a9334c4330492fed74b4d55f90496e337f4d5
Tiffany Chen <chen.qiq@northeastern.edu>
2024-04-01 21:41:47 -0700

    first commit

---
+++ src/main/java/com/example/mongoBlog/MongoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoBlogApplication.class, args);
+	}
+
+}
